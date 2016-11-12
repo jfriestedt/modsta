80e70c8ce5cf9f788b2129857a1e25cc0990bc1b
chrisruenes1 <ruenes.chris@gmail.com>
2016-11-12 14:58:37 -0500

    Add keys.js

---
+++ keys.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'C': ['C', 'd', 'e', 'F', 'G', 'a', 'bdim', 'C'],
+  'Db': ['Db', 'eb', 'f', 'Gb', 'Ab', 'bb', 'cdim', 'Db'],
+  'D': ['D', 'e', 'f#', 'G', 'A', 'b', 'c#dim', 'D'],
+  'Eb': ['Eb', 'f', 'g', 'Ab', 'Bb', 'c', 'ddim', 'Eb'],
+  'E': ['E', 'f#', 'g#', 'A', 'B', 'c#', 'd#dim', 'E'],
+  'F': ['F', 'g', 'a', 'Bb', 'C', 'd', 'edim', 'F'],
+  'Gb': ['Gb', 'ab', 'bb', 'Cb', 'Dd', 'eb', 'fdim', 'Gb'],
+  'G': ['G', 'a', 'b', 'C', 'D', 'e', 'f#dim', 'G'],
+  'Ab': ['Ab', 'bb', 'c', 'Db', 'Eb', 'f', 'gdim', 'Ab'],
+  'A': ['A', 'b', 'c#', 'D', 'E', 'f#', 'g#dim', 'A'],
+  'Bb': ['Bb', 'c', 'd', 'Eb', 'F', 'g', 'adim', 'Bb'],
+  'B': ['B', 'c#', 'd#', 'E', 'F#', 'g#', 'a#', 'B']
+};
