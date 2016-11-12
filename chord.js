622c3c51ad5b167887287b64f437f0ffdd496d70
Jon Friestedt <jfrieste@gmail.com>
2016-11-12 15:25:38 -0500

    Add chord.js

---
+++ chord.js
@@ -0,0 +1,27 @@
+import Keys from './keys';
+
+class Chord {
+  constructor (chordName, index, parentChord, numeral, quality, callbacks) {
+    this.chordName = chordName;
+    this.index = index;
+    this.parentChord = parentChord;
+    this.numeral = numeral;
+    this.quality = quality;
+    this.callbacks = callbacks;
+  }
+
+  getPossibleKeys () {
+    const possibleKeys = [];
+
+    for (var key in Keys) {
+      if (Keys.hasOwnProperty(key)) {
+        if (Keys[key].includes(this.chordName) &&
+            Keys[key].includes(this.parentChord.chordName)) {
+          possibleKeys.push(key);
+        }
+      }
+    }
+
+    return possibleKeys;
+  }
+}
